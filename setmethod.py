546ca0a77f29e9380a53f70424a2eb01af0dcb21
ankit-kumar-2004 <ankitsingh16533@gmail.com>
2026-03-11 11:57:09 +0530

    recursion

---
+++ setmethod.py
@@ -0,0 +1,19 @@
+'''collection = set()   # empty set
+
+collection.add(1)
+collection.add(2)
+collection.add(2)
+collection.add((1,2,3))
+#collection.remove(2)  # remove an element from the set
+collection.clear()  # remove all elements from the set
+print(collection)
+print(type(collection))'''
+
+
+
+
+
+set1={1,2,3,4,5}
+set2={4,5,6,7,8}
+print(set1.union(set2))  
+print(set1.intersection(set2))
